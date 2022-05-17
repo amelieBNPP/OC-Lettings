579f5d1e7ef6454c88fc81f8bc7f5c644444d661
amelieBNPP <amelie.noury@bnpparibas.com>
2022-05-17 16:23:27 +0200

    :package: refactor(config): delete migrations/apps files to clean config folder

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'config.apps.OCLettingsSiteConfig',
     'lettings.apps.LettingsConfig',
     'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
