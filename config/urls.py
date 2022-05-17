1676465bea8d3d8192cd46c0538ed9ce4aefa850
amelieBNPP <amelie.noury@bnpparibas.com>
2022-05-17 16:07:45 +0200

    :package: refactor(config): remove old models from admin and clean unused files

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
-from config import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TemplateView.as_view(template_name="index.html"), name='index'),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path('admin/', admin.site.urls),
